8ba81f3987beb758d1827cd76e5a767dca73c533
Lakshmi <lvnarke@gmail.com>
2018-10-16 12:03:08 +0530

    ...

--- cie_queries/src/sample/OCR.java
+++ cie_queries/src/sample/OCR.java
@@ -2,17 +2,8 @@ package sample;
 
 import com.jfoenix.controls.JFXToggleButton;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 
 public class OCR   {
